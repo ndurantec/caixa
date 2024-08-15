c954e2ee94d9047c4c28d9e86ee37fb3a0d33cdd
150Victor150 <victor.silva.oliveira@escola.pr.gov.br>
2024-08-15 11:23:18 -0300

    adiciano as classe uml

---
+++ src/main/java/com/castelo/caixa/modelo/Operacao.java
@@ -0,0 +1,52 @@
+package com.castelo.caixa.modelo;
+
+public class Operacao {
+    private String nome;
+
+    public Operacao() {
+    }
+
+    public Operacao(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Operacao [nome=" + nome + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Operacao other = (Operacao) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    
+}
