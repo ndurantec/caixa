ca7e852d85d303d2d89502df0758281a5580eb35
HendrywEduardo <hendryw.felten@escola.pr.gov.br>
2024-08-22 12:01:44 -0300

    adicionado Post Put Get Delete

--- src/main/java/com/castelo/caixa/controller/ContaController.java
+++ src/main/java/com/castelo/caixa/controller/ContaController.java
@@ -1,8 +1,11 @@
 package com.castelo.caixa.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +18,21 @@ public class ContaController {
     }
    
     @PostMapping(value = "/inserir")
-    public void inserir() {
+        public void inserir() {
+    
+    }
+
+    @PutMapping(value = "/atualizar")
+        public void listar(){
 
     }
 
+    @DeleteMapping(value = "/deletar")
+        public void deletar(){
+
+    }
+
+
 }
+
 
