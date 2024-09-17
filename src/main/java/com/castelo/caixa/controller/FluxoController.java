b99d700bd99b4ea55db2442867d38214f86a4bc7
150Victor150 <victor.silva.oliveira@escola.pr.gov.br>
2024-09-17 12:23:33 -0300

    arummado classes

--- src/main/java/com/castelo/caixa/controller/FluxoController.java
+++ src/main/java/com/castelo/caixa/controller/FluxoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.castelo.caixa.repository.OperacaoRepository;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/fluxo")
 public class FluxoController {
 
@@ -94,9 +96,7 @@ public class FluxoController {
 
     public void consultaEntrada(LocalDateTime data, String nome, Operacao origem) {
         
-    } 
-
-
+    }  
 
 
 
--- src/main/java/com/castelo/caixa/controller/OperacaoController.java
+++ src/main/java/com/castelo/caixa/controller/OperacaoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.castelo.caixa.modelo.Operacao;
 import com.castelo.caixa.repository.OperacaoRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/operacao")
 public class OperacaoController {
 
@@ -29,7 +31,7 @@ public class OperacaoController {
     @GetMapping (value = "/findAll")
     public List findAll(){
         return operacaoRepository.findAll();
-    }
+    }  
 
     @PostMapping (value = "/insert")
     public ResponseEntity<Operacao> insert(@RequestBody OperacaoDto operacaoDto) {
